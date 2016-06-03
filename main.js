2b92ddcbe548dd147e25d539094af1545711e17d
marcoflagello <marco.flagello@gmail.com>
2016-06-03 15:08:22 +0200

    avvio
    nuova prova

---
+++ main.js
@@ -0,0 +1,41 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+var fs = require('fs') ;
+var http = require('http') ;
+var dispatcher = require('./module-02') ;
+dispatcher.addListener("get", "/", function(req, res) {
+  fs.readFile(__dirname + '/home.html', function (err, content) {
+    if (err) {
+      res.writeHead(500) ;
+      return res.end('Error loading file') ;
+    }
+    res.writeHead(200) ;
+    res.end(content) ;
+  }) ;
+}) ;
+dispatcher.addListener("get", "/page1", function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'}) ;
+  res.end('Page One') ;
+}) ;
+dispatcher.addListener("get", "/page2", function(req, res) { 
+  res.writeHead(200, {'Content-Type': 'text/plain'}) ;
+  res.end('Page Two') ;
+}) ;
+dispatcher.addListener("post", "/login", function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'}) ;
+  res.end('Post one') ;
+}) ;
+
+var port = process_env.OPENSHIFT_NODEJS_PORT || 8080;
+var address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+
+http.createServer(function (req, res) {
+  dispatcher.dispatch(req, res) ;
+}).listen(8080, 'localhost') ;
+console.log("Server is up and running") ;
---
+++ module-02.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+var HttpDispatcher = function() {
+  this.leadingName = "Dispatcher >> " ;
+  this.list = { get: [], post: [] } ;
+  this.addListener = function(method, url, cb) {
+    this.list[method].push( {cb: cb, url: url} ) ;
+  }
+}
+HttpDispatcher.prototype.showList = function() {
+  console.log(this.leadingName + "Registered listeners :n") ;
+  for (method in this.list) {
+    console.log(method + " --> " + this.list[method].length + " listeners") ;
+    for (listener in this.list[method]) {
+      console.log(listener + " : " + this.list[method][listener].url + 
+      " --> " + this.list[method][listener].cb) ;
+    }
+  }
+}
+
+HttpDispatcher.prototype.dispatch = function(req, res) {
+  var parsedUrl = require('url').parse(req.url, true).pathname ;
+  var method = req.method.toLowerCase() ;
+  for (i in this.list[method]) if (this.list[method][i].url == parsedUrl) {
+    this.list[method][i].cb(req, res) ;
+  }
+}
+module.exports = new HttpDispatcher() ;
